a99f9582727421ccf11e5422e0b2d09cc74f4738
te9yie <te9yie@users.noreply.github.com>
2023-08-30 22:22:10 +0900

    Use SDL and ImGui

---
+++ src/main.cpp
@@ -0,0 +1,103 @@
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_sdlrenderer2.h"
+
+namespace {
+
+struct DestroyWindow {
+  void operator()(SDL_Window* w) const {
+    SDL_DestroyWindow(w);
+  }
+};
+using WindowPtr = std::unique_ptr<SDL_Window, DestroyWindow>;
+
+struct DestroyRenderer {
+  void operator()(SDL_Renderer* r) const {
+    SDL_DestroyRenderer(r);
+  }
+};
+using RendererPtr = std::unique_ptr<SDL_Renderer, DestroyRenderer>;
+
+}  // namespace
+
+int main(int /*argc*/, char* /*argv*/[]) {
+#if defined(_MSC_VER)
+  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+  SDL_LogSetAllPriority(SDL_LOG_PRIORITY_DEBUG);
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    SDL_LogCritical(SDL_LOG_CATEGORY_SYSTEM, "SDL_Init: %s", SDL_GetError());
+    return EXIT_FAILURE;
+  }
+  atexit(SDL_Quit);
+
+  const char* TITLE = "Game";
+  const int SCREEN_WIDTH = 16 * 60;
+  const int SCREEN_HEIGH = 9 * 60;
+  WindowPtr window(SDL_CreateWindow(TITLE, SDL_WINDOWPOS_UNDEFINED,
+                                    SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                                    SCREEN_HEIGH, SDL_WINDOW_RESIZABLE));
+  if (!window) {
+    SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "SDL_CreateWindow: %s",
+                    SDL_GetError());
+    return EXIT_FAILURE;
+  }
+
+  RendererPtr renderer(SDL_CreateRenderer(
+      window.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC));
+  if (!renderer) {
+    SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "SDL_CreateRenderer: %s",
+                    SDL_GetError());
+    return EXIT_FAILURE;
+  }
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+
+  ImGui_ImplSDL2_InitForSDLRenderer(window.get(), renderer.get());
+  ImGui_ImplSDLRenderer2_Init(renderer.get());
+
+  bool loop = true;
+  while (loop) {
+    {
+      SDL_Event e;
+      while (SDL_PollEvent(&e)) {
+        ImGui_ImplSDL2_ProcessEvent(&e);
+        switch (e.type) {
+          case SDL_QUIT:
+            loop = false;
+            break;
+          case SDL_WINDOWEVENT:
+            if (e.window.event == SDL_WINDOWEVENT_CLOSE &&
+                e.window.windowID == SDL_GetWindowID(window.get())) {
+              loop = false;
+            }
+            break;
+        }
+      }
+    }
+
+    int w, h;
+    SDL_GetRendererOutputSize(renderer.get(), &w, &h);
+
+    ImGui_ImplSDLRenderer2_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::Render();
+
+    SDL_SetRenderDrawColor(renderer.get(), 0x12, 0x34, 0x56, 0xff);
+    SDL_RenderClear(renderer.get());
+
+    ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
+
+    SDL_RenderPresent(renderer.get());
+  }
+
+  ImGui_ImplSDLRenderer2_Shutdown();
+  ImGui_ImplSDL2_Shutdown();
+  ImGui::DestroyContext();
+
+  return EXIT_SUCCESS;
+}
---
+++ src/pch.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#if defined(_MSC_VER)
+#include <crtdbg.h>
+#endif
+
+#include <SDL.h>
+
+#include <cstdlib>
+#include <memory>
+
+#include "imgui.h"
